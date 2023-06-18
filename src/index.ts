67b74458971b791184b55c882d32318906c9d9cf
Oein <oein0219@gmail.com>
2023-06-18 18:09:41 +0900

    text feature

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import prisma from "./utils/prisma";
 import { lzw_decode, lzw_encode } from "./utils/encode";
 import getWord from "./rand/getWord";
+import isbot from "isbot";
+
+import "dotenv/config";
 
 const app = express();
 
@@ -17,9 +20,30 @@ const randWordEng = (len: number) => {
 
 app.use(express.json());
 
+const botHandler = (req: any, res: any, link: string, ogTitle: string) => {
+  console.log(req.get("user-agent"));
+  if (isbot(req.get("user-agent"))) {
+    res.send({
+      s: false,
+      e: "<meta>",
+      meta: {
+        url: `https://poi.kr${link}`,
+        title: ogTitle,
+        description: ogTitle,
+      },
+    });
+    return true;
+  } else {
+    return false;
+  }
+};
+
 app.get("/url/redirect/:link", async (req, res) => {
   try {
     const link = req.params.link;
+
+    if (botHandler(req, res, `/${link}`, "Shorted link by someone / " + link))
+      return;
     let to = await prisma.link.findFirst({
       where: {
         from: lzw_encode(link),
@@ -27,7 +51,7 @@ app.get("/url/redirect/:link", async (req, res) => {
     });
     if (!to) return res.status(404).redirect("https://poi.kr/errors/link");
     if (to.expireOnce)
-      prisma.link.delete({
+      await prisma.link.delete({
         where: {
           from: lzw_encode(link),
         },
@@ -44,6 +68,15 @@ app.get("/url/redirect/:link", async (req, res) => {
 app.get("/custom/redirect/:link", async (req, res) => {
   try {
     const link = req.params.link;
+    if (
+      botHandler(
+        req,
+        res,
+        `/c/${link}`,
+        "Customized path link by someone / " + link
+      )
+    )
+      return;
     let to = await prisma.customLink.findFirst({
       where: {
         from: lzw_encode(link),
@@ -51,12 +84,71 @@ app.get("/custom/redirect/:link", async (req, res) => {
     });
     if (!to) return res.status(404).redirect("https://poi.kr/errors/custom");
     if (to.expireOnce)
-      prisma.customLink.delete({
+      await prisma.customLink.delete({
         where: {
           from: lzw_encode(link),
         },
       });
     return res.status(301).redirect(lzw_decode(to.to));
+  } catch (e) {
+    return res.status(500).json({
+      s: false,
+      e: "Internal Server Error",
+    });
+  }
+});
+
+app.get("/text/data/:link", async (req, res) => {
+  try {
+    const link = req.params.link;
+    if (botHandler(req, res, `/t/${link}`, "Shared text by someone / " + link))
+      return;
+    let to = await prisma.textLink.findFirst({
+      where: {
+        from: lzw_encode(link),
+      },
+    });
+    if (!to) return res.status(404).redirect("https://poi.kr/errors/text");
+    console.log(to);
+    if (to.expireOnce)
+      await prisma.textLink.delete({
+        where: {
+          from: lzw_encode(link),
+        },
+      });
+    return res.send({
+      s: true,
+      e: lzw_decode(to.text),
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      s: false,
+      e: "Internal Server Error",
+    });
+  }
+});
+
+app.get("/text/raw/:link", async (req, res) => {
+  try {
+    const link = req.params.link;
+    let to = await prisma.textLink.findFirst({
+      where: {
+        from: lzw_encode(link),
+      },
+    });
+    if (!to)
+      return res.status(404).send({
+        s: false,
+        e: "Not found",
+      });
+    if (to.expireOnce)
+      prisma.textLink.delete({
+        where: {
+          from: lzw_encode(link),
+        },
+      });
+    return res.send(lzw_decode(to.text));
   } catch (e) {
     return res.status(500).json({
       s: false,
@@ -148,6 +240,58 @@ app.post("/custom/new", async (req, res) => {
     return res.send({
       s: true,
       e: from,
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({
+      s: false,
+      e: "Internal Server Error",
+    });
+  }
+});
+
+app.post("/text/new", async (req, res) => {
+  let text = req.body.text;
+  let useOnce = req.body.useOnce;
+  let isEng = req.body.isEng;
+
+  if (
+    typeof text != "string" ||
+    typeof useOnce != "boolean" ||
+    typeof isEng != "boolean"
+  )
+    return res.send({
+      s: false,
+      e: "Invalid query",
+    });
+
+  try {
+    const getID = () => {
+      if (isEng) return randWordEng(6);
+      return getWord();
+    };
+    let id = getID();
+    while (true) {
+      let thing = await prisma.textLink.findFirst({
+        where: {
+          from: lzw_encode(id),
+        },
+      });
+      if (!thing) break;
+      id = getID();
+    }
+
+    await prisma.textLink.create({
+      data: {
+        from: lzw_encode(id),
+        text: lzw_encode(text),
+        expireOnce: useOnce,
+      },
+    });
+
+    return res.send({
+      s: true,
+      e: id,
     });
   } catch (e) {
     console.error(e);
